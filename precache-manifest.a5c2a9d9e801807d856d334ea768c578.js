9a04a88235cf96f6c6b0e5b2e6d6b451a9173d53
Vlada Antonchenko <vladajka16@gmail.com>
2020-08-19 13:26:58 -0400

    Updates

---
+++ precache-manifest.a5c2a9d9e801807d856d334ea768c578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4758d572d8eecbadbb28e437705f88ad",
+    "url": "/Employee_Direct/index.html"
+  },
+  {
+    "revision": "15b9c05bc9c7324fce53",
+    "url": "/Employee_Direct/static/css/main.51ce82f9.chunk.css"
+  },
+  {
+    "revision": "87faa65db5a033dab9d9",
+    "url": "/Employee_Direct/static/js/2.1a05e838.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee_Direct/static/js/2.1a05e838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b9c05bc9c7324fce53",
+    "url": "/Employee_Direct/static/js/main.b760bbd2.chunk.js"
+  },
+  {
+    "revision": "9f80ca76d6dfa5f0d1e3",
+    "url": "/Employee_Direct/static/js/runtime-main.257889b6.js"
+  }
+]);
